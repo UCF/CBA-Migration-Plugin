4bf681f527a0add4d80f66dc5b4a5c436c7b2dca
Jim Barnes <james.barnes@ucf.edu>
2017-03-27 12:20:14 -0400

    Updated to handle file uploads.

--- bot-import.php
+++ bot-import.php
@@ -10,6 +10,15 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
+
+	function my_allowed_mime_types( $mime_types ) {
+		$mime_types['jpg'] = 'image/jpeg';
+		$mime_types['pdf'] = 'application/pdf';
+		return $mime_types;
+	}
+
+	add_filter( 'upload_mimes', 'my_allowed_mime_types', 11, 1 );
+
 	require_once 'includes/class-bot-import.php';
 	require_once ABSPATH . 'wp-content/plugins/wordpress-importer/parsers.php';
 
--- includes/class-bot-import.php
+++ includes/class-bot-import.php
@@ -29,6 +29,7 @@ if ( ! class_exists( 'BOT_Import_Command' ) ) {
 			$people = array(),
 			$people_ids = array(),
 			$committees = array(),
+			$committee_ids = array(),
 			$meetings = array(),
 			$attachments = array(),
 			$attachment_urls = array(),
@@ -55,6 +56,7 @@ if ( ! class_exists( 'BOT_Import_Command' ) ) {
 						break;
 					case 'committee':
 						$this->committees[] = $post;
+						$this->committee_ids[(int)$post['post_id']] = $post;
 						break;
 					case 'meeting':
 						$this->meetings[] = $post;
@@ -76,12 +78,18 @@ if ( ! class_exists( 'BOT_Import_Command' ) ) {
 				}
 			}
 			
-			//$this->import_people();
+			$this->import_people();
 			$this->import_committees();
 			//$this->import_meetings();
+
+			WP_CLI::success( 'Finished importing! Have a nice day.' );
 		}
 
 		private function import_people() {
+			$count = count( $this->people );
+			$import_count = 0;
+			$progress = \WP_CLI\Utils\make_progress_bar( 'Importing people...', $count );
+
 			foreach( $this->people as $person ) {
 				$name = $person['post_title'];
 				$post_id = post_exists( $name );
@@ -137,40 +145,58 @@ if ( ! class_exists( 'BOT_Import_Command' ) ) {
 					if ( $thumbnail ) {
 						$this->add_attachment( $thumbnail, $post_id, '_thumbnail_id' );
 					}
+
+					$import_count++;
 				}
 
 				$this->people_ids[$person['post_id']] = $post_id;
+				$progress->tick();
 			}
+
+			$progress->finish();
+			WP_CLI::success( 'Successfully imported ' . $import_count . ' people.' );
 		}
 
 		private function import_committees() {
+			$count = count( $this->committees );
+			$import_count = 0;
+			$progress = \WP_CLI\Utils\make_progress_bar( 'Importing committees...', $count );
+
 			foreach( $this->committees as $committee ) {
 				$name = $committee['post_title'];
-				if ( ! term_exists( $name, 'people_group' ) ) {
-					// Get the post meta
-					$metas = array();
-
-					foreach( $committee['postmeta'] as $meta ) {
-						$metas[$meta['key']] = $meta['value'];
-					}
-
+				$term = term_exists( $name, 'people_group' );
+				$import = false;
+				if ( ! $term ) {
 					$term = wp_insert_term( $name, 'people_group', array(
 						'description' => $metas['committee_description']
 					) );
 
-					// $members = maybe_unserialize( $metas['committee_members'] );
-					// $staff = maybe_unserialize( $metas['committee_staff'] );
+					$import = true;
 
-					// foreach( $members as $id=>$title ) {
-					// 	$post_id = $this->people_ids[$id];
-					// 	wp_set_post_terms( $post_id, $term, 'people_group', TRUE );
-					// }
+					$import_count++;
+				}
 
-					// foreach( $staff as $id=>$title ) {
-					// 	$post_id = $this->people_ids[$id];
-					// 	wp_set_post_terms( $post_id, $term, 'people_group', TRUE );
-					// }
+				// Get the post meta
+				$metas = array();
 
+				foreach( $committee['postmeta'] as $meta ) {
+					$metas[$meta['key']] = $meta['value'];
+				}
+
+				$members = maybe_unserialize( $metas['committee_members'] );
+				$staff = maybe_unserialize( $metas['committee_staff'] );
+
+				foreach( $members as $id=>$title ) {
+					$post_id = $this->people_ids[$id];
+					wp_set_post_terms( $post_id, $term['term_id'], 'people_group', TRUE );
+				}
+
+				foreach( $staff as $id=>$title ) {
+					$post_id = $this->people_ids[$id];
+					wp_set_post_terms( $post_id, $term['term_id'], 'people_group', TRUE );
+				}
+
+				if ( $import ) {
 					$document_id = (int)$metas['committee_charter'];
 					$attachment_id = $this->documents[$document_id];
 					$attachment = $this->attachments[$attachment_id];
@@ -179,11 +205,85 @@ if ( ! class_exists( 'BOT_Import_Command' ) ) {
 						$this->add_attachment( $attachment, (int)$term['term_id'], 'people_group_charter', FALSE );
 					}
 				}
+
+				$this->committee_ids[$committee['post_id']] = $term->term_id;
+				$progress->tick();
 			}
+
+			// Insert 'None' Committee option if it does not exist.
+			if ( ! term_exists( 'None', 'people_group' ) ) {
+				wp_insert_term( 'None', 'people_group', array(
+					'description' => 'Use for meetings that are not tied to a particular committee.'
+				) );
+			}
+
+			$progress->finish();
+			WP_CLI::success( 'Successfully imported ' . $import_count . ' committees.' );
 		}
 
 		private function import_meetings() {
+			$count = count( $this->meetings );
+			$import_count = 0;
+			$progress = \WP_CLI\Utils\make_progress_bar( 'Importing meetings...', $count );
+			$none_term = term_exists( 'None', 'people_group' );
 
+			foreach( $this->meetings as $meeting ) {
+				$name = $meeting['post_title'];
+				$post_id = post_exists( $name );
+				if ( ! $post_id && $meeting['status'] === 'publish' ) {
+					$content = $meeting['post_content'];
+					$metas = array();
+
+					foreach( $meeting['postmeta'] as $meta ) {
+						$metas[$meta['key']] = $meta['value'];
+					}
+
+					$committee = $this->committee_ids[(int)$metas['meeting_committee']]['post_title'];
+					$agenda = $this->attachments[(int)$metas['meeting_agenda']];
+					$minutes = $this->attachments[(int)$metas['meeting_minutes']];
+					$location = $metas['meeting_location'];
+					$date = new DateTime( $metas['meeting_date'] );
+					$start_time = new DateTime( $metas['meeting_start_time'] );
+					$end_time = new DateTime( $metas['meeting_end_time'] );
+
+					$post_id = wp_insert_post( array(
+						'post_title'             => $name,
+						'post_content'           => $content,
+						'post_type'              => 'meeting',
+						'post_status'            => 'publish',
+						'meta_input'             => array(
+							'ucf_meeting_date'       => $date->format( 'Y-m-d' ),
+							'ucf_meeting_start_time' => $start_time->format( 'H:i' ),
+							'ucf_meeting_end_time'   => $end_time->format( 'H:i' ),
+							'ucf_meeting_location'   => $location
+						)
+					) );
+
+					if ( $committee ) {
+						$term = term_exists( $committee, 'people_group' );
+						if ( $term ) {
+							wp_set_post_terms( $post_id, $term, 'people_group', TRUE );
+						}
+					} else {
+						if ( $none_term ) {
+							wp_set_post_terms( $post_id, $none_term, 'people_group', TRUE );
+						}
+					}
+
+					if ( $agenda ) {
+						$this->add_attachment( $agenda, $post_id, 'ucf_meeting_agenda' );
+					}
+
+					if ( $minutes ) {
+						$this->add_attachment( $minutes, $post_id, 'ucf_meeting_minutes' );
+					}
+					$import_count++;
+				}
+				$progress->tick();
+			}
+
+			$progress->finish();
+			WP_CLI::success( 'Successfully imported ' . $import_count . ' meetings.' );
 		}
 
 		private function add_attachment( $attachment, $id, $meta_key, $post=TRUE ) {
@@ -198,19 +298,33 @@ if ( ! class_exists( 'BOT_Import_Command' ) ) {
 				return;
 			}
 
-			preg_match('/[^\?]+\.(jpg|jpe|jpeg|gif|png)/i', $url, $matches);
-			$file_array['name'] = basename( $matches[0] );
+			if ( preg_match('/[^\?]+\.(jpg|jpe|jpeg|gif|png)/i', $url, $matches) ) {
+				$file_array['name'] = $matches[0];
+			} else {
+				$file_array['name'] = $attachment['post_title'];
+			}
+			
 			$file_array['tmp_name'] = $tmp;
 
-			preg_match( '%^[0-9]{4}/[0-9]{2}%', $url, $matches );
-			$file_array['upload_date'] = $matches[0];
+			$filetype = wp_check_filetype( basename( $url ), null );
+			$file_array = array_merge( $file_array, $filetype );
 
-			$attachment_id = media_handle_sideload( $file_array, $post_id, $attachment['post_title'] );
+			if ( preg_match( '%[0-9]{4}/[0-9]{2}%', $url, $matches ) ) {
+				$file_array['upload_date'] = $matches[0];
+			}
 
-			if ( $id && $post ) {
-				update_post_meta( $id, $meta_key, $attachment_id );
+			$attachment_id = media_handle_sideload( $file_array, $post_id );
+
+			if ( is_wp_error( $attachment_id ) ) {
+                @unlink( $file_array['tmp_name'] );
+				WP_CLI::warning( 'Unable to upload ' . $file_array['name'] . '.' );
+			}
+
+			if ( $id && $post && ! is_wp_error( $attachment_id ) ) {
+				update_field( $meta_key, $attachment_id, $id );
 			} else if ( $id && ! is_wp_error( $attachment_id ) ) {
 				// Use ACF's update_field
+				$id = 'people_group_' . $id;
 				update_field( $meta_key, $attachment_id, $id );
 			}
 		}
